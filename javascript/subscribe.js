2a0953d12e2ec23c9f0fb65d21f59d7b0e65c6d8
Sergio Radigales <sergioradigales@gmail.com>
2022-04-18 16:15:50 +0200

    newsletter with post

--- javascript/subscribe.js
+++ javascript/subscribe.js
@@ -1,6 +1,5 @@
 const form_sec = document.getElementById("subs");
 const formu = document.getElementById("subscribe");
-const email = document.getElementById("emailValue").value;
 const closer = document.getElementById("close");
 const bb = document.getElementsByTagName("body");
 
@@ -12,4 +11,34 @@ setTimeout(showPopUp, 5000);
 
 closer.addEventListener("click", () => {
   form_sec.style.transform = "translateX(-100%)";
+});
+
+formu.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("emailValue").value;
+  let regex = /@/;
+  const result = regex.test(email);
+  console.log(result);
+
+  if (result === false) {
+    alert("That's not an email");
+    return;
+  }
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+  formu.reset();
+
+  setTimeout(() => {
+    form_sec.style.transform = "translateX(-100%)";
+  }, 2000);
 });
